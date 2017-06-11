ab2327f82d83e0795da96f3acd5c0e00c47ec743
Luiz Fernando <lfernando07@outlook.com>
2017-06-11 23:13:10 +0000

    Criei a pagina de Login

---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Login</title>
+            <link rel="stylesheet" href="trabalhoGarcia/normalize/normalize.css">
+            <link rel="stylesheet" href="trabalhoGarcia/css/uikit.min.css">
+            <link rel="stylesheet" href="trabalhoGarcia/css/style.css" type="text/css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+            <script src="trabalhoGarcia/js/uikit.min.js"></script>
+            <script src="trabalhoGarcia/js/uikit-icons.min.js"></script>
+            <script type="text/javascript" src="trabalhoGarcia/js/p2p.js"></script>
+            <script type="text/javascript" src="trabalhoGarcia/ajax.js"></script>
+        </head>
+        <body>
+
+            <div class="uk-text-center" uk-grid>
+                <header class="uk-width-1@s uk-width-1@m uk-width-1-2@l">
+                    <picture>
+                        <img src="trabalhoGarcia/img/logo.png" alt=""></img>
+                    </picture>
+                </header>
+                <div class="uk-visible@l uk-width-1-2@l back">
+                    
+                </div>
+            </div>
+            
+            <div uk-grid style="margin-top:0px;min-height:747px;">
+                <aside class="uk-visible@l uk-width-1-2@l">
+                    <h1>Bem-Vindo</h1>
+                    <h4>Um maneira de conectar as pessoas.</h4>
+                    <blockquote>
+                        <p>Doar é um dos elos capazes de fortalecer a corrente do bem. Todo mundo pode dar algo de si e ganhar muito com isso. Incentive gestos solidários.</p>
+                    </blockquote>
+                    <picture>
+                        <img src="trabalhoGarcia/img/backgroundLogin.png" alt="A economia colaborativa conectando as coisas">
+                    </picture>
+                </aside>
+                <main class="uk-width-1@s uk-width-1@m uk-width-1-2@l back">
+                    <h2>Entrar</h2>
+                </main>
+            </div>
+            <footer class="" uk-grid>
+                    <p class="uk-width-1@s uk-width-1@m uk-width-1@l footer">&copy; Economia Colaborativa</p>
+            </footer>
+        </body>
+    </html>
