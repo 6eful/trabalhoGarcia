18de51c9752d3e09b076e6a685b8ce95f0b0851c
Luiz Fernando <lfernando07@outlook.com>
2017-06-11 18:02:26 +0000

    Função para pegar todos os produtos no banco de dados

---
+++ app/Produto.php
@@ -0,0 +1,78 @@
+<?php
+
+interface ProdutoResource{
+    function manipular($id);
+    function todos();
+} 
+
+class ProdutoResourcePOST implements ProdutoResource{
+    
+   public function manipular($id=0){
+        header("Access-Control-Allow-Origin: *");
+        $accept = $_SERVER["CONTENT_TYPE"];
+        echo $accept;
+        // if($accept === "application/json"){
+            $json = file_get_contents('php://input');
+            $obj = json_decode($json);
+            $ndao = new ProdutoDAO();
+            $ndao->inserir($obj);
+            echo "Ok!";
+        // }else{
+        //     echo "Headers inválidos";
+        //     http_response_code(400);
+        // }
+    }
+    
+    public function todos(){
+        //Todos eh GET e nao POST
+        echo "Error";
+        http_response_code(405);
+    }
+    
+    public function __call($m, $arg){
+        echo "$m nao achado para POST";
+        http_response_code(404);
+    }
+}
+
+class ProdutoResourceGET implements ProdutoResource{
+    
+    public function manipular($id=3){
+        $ndao = new ProdutoDAO();
+        echo $ndao->getProduto($id);
+    }
+    
+    public function todos(){
+        //header("Access-Control-Allow-Origin: *");
+        $ndao = new ProdutoDAO();
+        echo $ndao->listar();
+    }
+    
+    public function __call($m, $arg){
+        echo "$m nao achado para GET";
+        http_response_code(404);
+    }
+}
+
+class ProdutoResourceOPTIONS implements ProdutoResource{
+    
+    public function __call($m, $arg){
+        header("Access-Control-Allow-Origin: *");
+        echo "$m nao achado para OPTIONS";
+        http_response_code(404);
+    }
+    
+    public function manipular($id=1){
+        header("Access-Control-Allow-Origin: *");
+        header('Allow: OPTIONS, GET, POST, DELETE, PUT');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
+    }
+    
+    public function todos(){
+        //COLOCAR OS METODOS QUE RETORNAM RESPONSES POSITIVOS (NAO-ERRO)
+        header('Allow: OPTIONS, GET, POST, DELETE, PUT');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
+    }
+}
+
+?>
---
+++ app/model/ProdutoDAO.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+class ProdutoDAO{
+    
+    private $conn;
+    
+    public function __construct(){
+        $this->conn = Connection::getConn("127.0.0.1", "a6eful", "", "DataBase", 3306);
+    }
+    public function listar(){
+            $st = $this->conn->prepare("SELECT p.title,p.url_imagem, DATE_FORMAT(dt_publicacao,  '%d/%m/%y') , u.nm_Usuario, p.ds_produto FROM Produto as p inner join Usuario as u on p.cd_usuario = u.Cd_Usuario ") or die("1".$conn->error);
+            $st->execute() or die("2".$st->error);
+            $st->bind_result($col0,$col1,$col2,$col4,$col5);
+            $arrMaior = array();
+            $arrMeio = array();
+            $novo;
+            while($st->fetch()){
+                $arrMeio[] = array(
+                                    "nome"      =>  $col0,
+                                    "url"       =>  $col1,
+                                    "data"      =>  $col2,
+                                    "por"       =>  $col4,
+                                    "descricao" =>  $col5);
+            }
+            $arrMaior["resp"] = $arrMeio;
+            echo json_encode($arrMaior);
+    }
+}
+?>
